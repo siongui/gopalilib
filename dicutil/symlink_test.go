7ff226642ea48855ba710b15d074e94ed78dbc3a
siongui <siongui@gmail.com>
2017-05-06 02:48:37 +0800

    fix name error

--- dicutil/symlink_test.go
+++ dicutil/symlink_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestGensite(t *testing.T) {
+func TestSymlinkToRootIndexHtml(t *testing.T) {
 	err := SymlinkToRootIndexHtml("../../pali/go/website/json/", "../../pali/go/website/")
 	if err != nil {
 		t.Error(err)
