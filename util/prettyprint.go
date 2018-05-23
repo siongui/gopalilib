134127a3cd131e87d2b29994154d529ba6d6cab4
siongui <siongui@gmail.com>
2018-05-23 21:51:08 +0800

    update pretty print

--- util/prettyprint.go
+++ util/prettyprint.go
@@ -2,10 +2,14 @@ package util
 
 import (
 	"encoding/json"
+	"fmt"
 )
 
 // Pretty print Go variable (struct, map, array, slice, etc.).
-func PrettyPrint(v interface{}) {
-	b, _ := json.MarshalIndent(v, "", "  ")
-	println(string(b))
+func PrettyPrint(v interface{}) (err error) {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err == nil {
+		fmt.Println(string(b))
+	}
+	return
 }
