0081620f70df4cf2254c385b12700efd01573740
siongui <siongui@gmail.com>
2020-07-26 02:52:50 +0800

    parse tipitaka_toc.xml

--- tpkutil/downloadtpk.go
+++ tpkutil/downloadtpk.go
@@ -3,11 +3,20 @@ package tpkutil
 // Download Tipiṭaka xml from https://www.tipitaka.org/romn/
 
 import (
+	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"path"
 
 	"github.com/siongui/gopalilib/util"
 )
+
+type Tree struct {
+	XMLName xml.Name `xml:"tree"`
+	Trees   []Tree   `xml:"tree"`
+	Text    string   `xml:"text,attr"`
+	Src     string   `xml:"src,attr"`
+}
 
 // DownloadTipitaka downloads all Tipiṭaka XMLs from
 // https://www.tipitaka.org/romn/ to dir. This method will overwrite existing
@@ -23,10 +32,26 @@ func DownloadTipitaka(dir string, overwrite bool) (err error) {
 	rootTocXmlSrc := "tipitaka_toc.xml"
 
 	srcXml := urlPrefix + rootTocXmlSrc
-	fmt.Println(srcXml)
+	//fmt.Println(srcXml)
 	dstXml := path.Join(dir, rootTocXmlSrc)
-	fmt.Println(dstXml)
+	//fmt.Println(dstXml)
 
 	err = util.CheckDownload(srcXml, dstXml, overwrite)
+	if err != nil {
+		return
+	}
+	b, err := ioutil.ReadFile(dstXml)
+	if err != nil {
+		return
+	}
+
+	tree := Tree{}
+	err = xml.Unmarshal(b, &tree)
+	if err != nil {
+		return
+	}
+
+	fmt.Println(tree)
+	util.PrettyPrint(tree)
 	return
 }
